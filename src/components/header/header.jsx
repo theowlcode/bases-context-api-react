67eaf46108fe830a54147d87cf02724fb4ae4b55
alejandro-cardenas-g <alejandro.cardenas.g07@gmail.com>
2023-05-19 17:30:13 -0500

    start project

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <span>Mi tienda</span>
+      </div>
+      <div>
+        <span>Ver carrito 0</span>
+      </div>
+    </div>
+  );
+};
